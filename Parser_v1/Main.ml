e166791c4a64d476a05a3147a872b7f78f7d93e1
Estelle <estelle@mhmac.scss.tcd.ie>
2019-07-09 14:19:40 +0100

    Emv well formed check

--- Parser_v1/Main.ml
+++ Parser_v1/Main.ml
@@ -13,9 +13,15 @@ let report_error filename lexbuf msg =
   let filebuf = Lexing.from_channel input in
   try
   let ast = Parser.main Lexer.token filebuf  in
- (*  print_string (string_of_ast ast); *)
-  let (envType,envVar) = create_env ([],[]) ast in
-  well_formed_envType envType 
+  print_string ("File parsed \n");
+  (* print_string (string_of_ast ast); *)
+  let (envType,envVar,start) = create_env ([],[],None) ast in
+  let (envType,envVar) = (List.rev envType, List.rev envVar) in
+  print_string ("Environment created \n");
+  let nameListType = (well_formed_envType envType) in
+  let (_,nameListFunc) = (well_formed_envVar envVar nameListType) in
+  let _ = well_formed_start start nameListFunc in
+  print_string ("Environment well-formed")
   with
   | Lexer.Error s ->
       report_error file filebuf "lexical error (unexpected character).";
@@ -27,9 +33,6 @@ let report_error filename lexbuf msg =
     let c = pos.pos_cnum - pos.pos_bol + 1 in
     Printf.eprintf "File \"%s\", line %d, character %d: The type %s is declared twice\n" file pos.pos_lnum c name;
     exit 2;
- | Resolve.Unknow_error_in_type_checking ->
-    Printf.eprintf "Fatal error in type checking";    
-    exit 2
  | Resolve.Undeclared_type (pos,name) ->
     let c = pos.pos_cnum - pos.pos_bol + 1 in
     Printf.eprintf "File \"%s\", line %d, character %d: Incorrect declaration type: unbound value %s\n" file pos.pos_lnum c name;
@@ -37,5 +40,24 @@ let report_error filename lexbuf msg =
  | Resolve.Unauthorized_recursivity pos ->
     let c = pos.pos_cnum - pos.pos_bol + 1 in
     Printf.eprintf "File \"%s\", line %d, character %d: Incorrect declaration type: only channel type can be recursive\n" file pos.pos_lnum c;
+    exit 2
+ | Resolve.Multiple_declaration_var (pos,name) ->
+    let c = pos.pos_cnum - pos.pos_bol + 1 in
+    Printf.eprintf "File \"%s\", line %d, character %d: The name %s is already used by another function/variable\n" file pos.pos_lnum c name;
+    exit 2;
+ | Resolve.Multiple_declaration_param (pos,name) ->
+    let c = pos.pos_cnum - pos.pos_bol + 1 in
+    Printf.eprintf "File \"%s\", line %d, character %d: The name %s is used twice in the function paraneters\n" file pos.pos_lnum c name;
+    exit 2;
+ | Resolve.Type_not_found (pos,name) ->
+    let c = pos.pos_cnum - pos.pos_bol + 1 in
+    Printf.eprintf "File \"%s\", line %d, character %d: Incorrect type: unbound value %s\n" file pos.pos_lnum c name;
+    exit 2;
+ | Resolve.Function_not_found (pos,name) ->
+    let c = pos.pos_cnum - pos.pos_bol + 1 in
+    Printf.eprintf "File \"%s\", line %d, character %d: Incorrect function for start: unbound value %s\n" file pos.pos_lnum c name;
+    exit 2;
+ | Resolve.Unknow_error_in_type_checking ->
+    Printf.eprintf "Fatal error in type checking";    
     exit 2
       
--- Parser_v1/Resolve.ml
+++ Parser_v1/Resolve.ml
@@ -6,19 +6,25 @@ exception Unbound_value of Lexing.position * string
 exception Undeclared_type of Lexing.position * string
 exception Unauthorized_recursivity of Lexing.position
 exception Multiple_declaration_var of Lexing.position * string
+exception Multiple_declaration_param of Lexing.position * string
+exception Type_not_found of Lexing.position * string
+exception Function_not_found of Lexing.position * string
+exception Start_not_found
 
-let rec create_env (envType,envVar) tree =
+let rec create_env (envType,envVar,start) tree =
     match tree with
-        | (ProgramNode (p1,p2)) -> 
-      create_env (create_env (envType,envVar) p1) p2
-        | (FunctionNode (_,_,_,_,_)) ->
-      (envType,tree::envVar)
-        | (VariableDeclaNode (_,_,StringNode(_,_))) -> 
-      (envType,tree::envVar)      
-        | (TypeDeclaNode (_,_,_)) ->
-        (tree::envType,envVar)
+        | ProgramNode (p1,p2) -> 
+      create_env (create_env (envType,envVar,start) p1) p2
+        | FunctionNode (_,_,_,_,_) ->
+      (envType,tree::envVar,start)
+        | VariableDeclaNode (_,_,StringNode(_,_)) -> 
+      (envType,tree::envVar,start)      
+        | TypeDeclaNode (_,_,_) ->
+      (tree::envType,envVar,start)
+        | CallNode (_,_,_) ->
+      (envType,envVar,Some(tree)) 
         | _ -> 
-      (envType,envVar)
+      (envType,envVar,start)
 
 let rec uniqueType envType nameList = 
   match envType with
@@ -62,20 +68,51 @@ let well_formed_type_decla decla nameList =
 
 let well_formed_envType envType =
     let nameList = uniqueType envType [] in
-    List.for_all (fun d -> well_formed_type_decla d nameList) envType
+    let _ = List.for_all (fun d -> well_formed_type_decla d nameList) envType in
+    nameList
 
-let rec uniqueVar envVar nameListVar nameListType = 
+let rec uniqueVar envVar nameListVar nameListType nameListFunc = 
   match envVar with
-    | [] -> nameListVar
-    | VariableDeclaNode(pos,t,StringNode(_,name))::q -> if (List.mem name nameListVar) 
+    | [] -> (nameListVar,nameListFunc)
+    | VariableDeclaNode (pos,_,StringNode(_,name))::q -> if (List.mem name nameListVar) 
                          then raise (Multiple_declaration_var (pos,name))                   
-                         else let b = (well_formed_type_aux t nameListType) in 
-                         (uniqueVar q (name::nameList))
-    | VariableDeclaNode(pos,name,_)::q -> if (List.mem name nameList) 
+                         else uniqueVar q (name::nameListVar) nameListType nameListFunc
+    | FunctionNode (pos,_,name,_,_)::q -> if (List.mem name nameListVar) 
                          then raise (Multiple_declaration_var (pos,name))                   
-                         else (uniqueVar q (name::nameList))
+                         else uniqueVar q (name::nameListVar) nameListType (name::nameListFunc)
     | _ -> raise Unknow_error_in_type_checking
 
-let well_formed_envVar envVar =
-    let nameList = uniqueType envType [] in
-    List.for_all (fun d -> well_formed_type d nameList) envType
+let well_formed_func_type t nameList = 
+  match t with
+    | FuncTNode (None) -> true
+    | FuncTNode (Some(st)) -> well_formed_type_aux st nameList 
+    | _ -> raise Unknow_error_in_type_checking
+
+let rec well_formed_params params nameListType nameListVar =
+  match params with 
+    | ParamsNode (pos,t,name,None) -> if (List.mem name nameListVar) 
+                  then raise (Multiple_declaration_param (pos,name))
+                  else let _ = (well_formed_type_aux t nameListType) in name::nameListVar
+    | ParamsNode (pos,t,name,Some(p)) -> if (List.mem name nameListVar) 
+                  then raise (Multiple_declaration_param (pos,name))
+                  else let _ = (well_formed_type_aux t nameListType) in well_formed_params p nameListType (name::nameListVar)
+    | _ -> raise Unknow_error_in_type_checking 
+
+let well_formed_var decla nameListType =
+  match decla with
+    | VariableDeclaNode (_,t,_) -> well_formed_type_aux t nameListType 
+    | FunctionNode (_,ft,_,params,_) -> let _ = (well_formed_params params nameListType []) in (well_formed_func_type ft nameListType)
+    | _ -> raise Unknow_error_in_type_checking
+
+let well_formed_envVar envVar nameListType =
+    let names = uniqueVar envVar [] nameListType [] in
+    try 
+      let _ = List.for_all (fun d -> well_formed_var d nameListType) envVar in 
+      names
+    with
+      | Unbound_value (pos,n) -> raise (Type_not_found (pos,n))
+      
+let well_formed_start decla nameListFunc =
+  match decla with 
+    | Some(CallNode (pos,name,_)) -> if (List.mem name nameListFunc) then true else raise (Function_not_found (pos,name))
+    | _ -> raise Unknow_error_in_type_checking  
