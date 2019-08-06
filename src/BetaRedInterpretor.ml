fc1d64dde5d5f47eacd510ac2acf03b8531df2d8
Estelle <estelle@mhmac.scss.tcd.ie>
2019-08-06 19:46:12 +0100

    BetaRedInterpretor working

---
+++ src/BetaRedInterpretor.ml
@@ -0,0 +1,242 @@
+open Ast
+open ExpressionInterpretor
+open Stack
+open Hashtbl
+
+type state = (string, valueType ref) Hashtbl.t
+
+type frame = 
+  | FuncCallReturnFrame of state * string
+  | FuncCallVoidFrame of state
+  | InstrSeqFrame of ast
+
+let envType = ref([(Lexing.dummy_pos,"",NoopNode,false)])
+let g = ref (Hashtbl.create 100)
+let s = ref (Hashtbl.create 100)
+let e = ref (Stack.create ()) 
+
+exception Unknown_error_reference_interpretor_ of (frame option * ast)
+
+(* def_value: ast -> valueType
+Function returning the default value for a type
+Parameter:
+  - t: the abstract syntax tree representing the type
+Return: the default value for the type passed in parameter
+*)
+let rec def_value t =
+  match t with
+    | TypeNode (_,IntegerT) -> IntegerVal(0)
+    | TypeNode (_,BooleanT) -> BooleanVal(true)
+    | TypeNode (_,StringT) -> StringVal("")
+    | TypeNode (_,CharT) -> CharVal("")
+    | ListTNode (_,_) -> ListVal([])
+    | TupleTNode (_,TypeSeqNode(st,None)) -> def_value st
+    | TupleTNode (_,tSeq) -> TupleVal (
+      let rec aux t l = match t with
+        | TypeSeqNode (st,None) -> List.rev ((def_value st)::l) 
+        | TypeSeqNode (st1, Some(st2)) -> aux st2 ((def_value st1)::l)
+        | _ -> raise (Unknown_error_reference_interpretor "def_value2")
+      in aux tSeq [])
+    | NamedTypeNode (_,st) -> let _,_,t,_ = (List.find (fun (_,name,_,_) -> String.equal name st) !envType) in (def_value t)
+    | _ -> raise (Unknown_error_reference_interpretor "def_value")
+
+
+(* extend_state_with_params: ast -> (string, valueType ref) Hashtbl.t -> valueType -> unit
+Function adding the parameters of a function to the state 
+Parameters:
+  - paramsnode: the abstract syntax tree representing the paramerers
+  - state: hash table representing the state of all the variables
+  - vparams: the value of the parameters
+*)
+let extend_state_with_params paramsnode state vparams =
+  let vp = (match vparams with 
+    | TupleVal(l) -> l
+    | e -> [e]
+  ) in
+  let rec aux pnode vpa =
+    match (pnode,vpa) with
+      | (ParamsNode (_,_,name,None), [e])-> Hashtbl.add state name (ref e)
+      | (ParamsNode (_,_,name,Some(p)), t::q) -> Hashtbl.add state name (ref t); aux p q
+      | _ -> raise (Unknown_error_reference_interpretor "extend_state_with_params2")
+  in aux paramsnode vp
+
+
+(* extend_state_with_local_declas: ast -> (string, valueType ref) Hashtbl.t -> unit
+Function adding the local variable of a function to the state 
+Parameters:
+  - body: the abstract syntax tree representing the function body
+  - state: hash table representing the state of all the variables
+*)  
+let extend_state_with_local_declas decla state =
+  match decla with
+    | None -> ()
+    | Some(d) -> 
+      let rec aux d =
+        (match d with
+          | VariableDeclasNode (VariableDeclaNode (_,t,name),None) -> Hashtbl.add state name (ref(def_value t)) 
+          | VariableDeclasNode (VariableDeclaNode (_,t,name),Some(vs)) -> Hashtbl.add state name (ref(def_value t)); aux vs
+          | _ -> raise (Unknown_error_reference_interpretor "extend_state_with_local_declas2")
+        )
+      in aux d
+
+
+(* create_local_state: ast -> ast -> (string, valueType ref) Hashtbl.t -> valueType -> (string, valueType ref) Hashtbl.t
+Function creating a new local state containing the parameters, the global and local variables 
+Parameters:
+  - params: the abstract syntax tree representing the paramerers
+  - body: the abstract syntax tree representing the function body
+  - state: hash table representing the state of all the variables
+  - vparams: the value of the parameters
+Return: the new local state
+*)  
+let create_state params decla vparams =
+  let local = Hashtbl.create 100 in
+  extend_state_with_params params local vparams;
+  extend_state_with_local_declas decla local;
+  local
+
+(* Numero 8 *)
+let ruleSeqInstr ast =
+  match ast with 
+    | InstrSeqNode(i,None) -> i
+    | InstrSeqNode(i,Some(iseq)) -> push (InstrSeqFrame(iseq)) (!e); i
+    | _ -> raise (Unknown_error_reference_interpretor "ruleSeqInstr")
+
+(* Numero 10 *)
+let ruleAssignInstr a expr =
+  match a with 
+    | AssignNode (_,name) ->
+      let ve = value_of_expr expr (!g,!s) in
+      let pa = find (!s) name in
+      pa := ve;
+      NoopNode
+    | _ -> raise (Unknown_error_reference_interpretor "ruleAssignInstr")
+
+(* Numero 1 *)
+let ruleCallFuncWithReturn a namef expr = 
+  let f = !(find !g namef) in
+  let ve = value_of_expr expr (!g,!s) in
+  match (f,a) with 
+    | (FuncVal(param,BodyNode(_,decla,instr)),AssignNode(_,name)) ->
+      push (FuncCallReturnFrame(copy (!s),name)) (!e);
+      s := create_state param decla ve;
+      instr 
+    | _ -> raise (Unknown_error_reference_interpretor "ruleCallFuncWithReturn") 
+
+(* Numero 2 *)
+let ruleCallFuncVoid namef expr = 
+  let f = !(Hashtbl.find !g namef) in
+  let ve = value_of_expr expr (!g,!s) in
+  match f with 
+    | FuncVal(param,BodyNode(_,decla,instr)) ->
+      push (FuncCallVoidFrame(copy (!s))) (!e);
+      s := create_state param decla ve;
+      instr 
+    | _ -> raise (Unknown_error_reference_interpretor "ruleCallFuncVoid")
+
+(* Numero / *)
+let ruleIfThenElseInstr cond i1 i2 =
+  let vcond = value_of_expr cond (!g,!s) in
+  match vcond with
+  | BooleanVal(true) -> i1
+  | BooleanVal(false) -> i2
+  | _ -> raise (Unknown_error_reference_interpretor "ruleIfThenElseInstr")
+
+(* Numero 11 et 12 *)
+let ruleWhile pos cond instr =
+  let vcond = value_of_expr cond (!g,!s) in
+  match vcond with
+  | BooleanVal(true) -> InstrSeqNode(instr,Some(WhileNode(pos,cond,instr)))
+  | BooleanVal(false) -> NoopNode
+  | _ -> raise (Unknown_error_reference_interpretor "ruleWhile")
+
+(* Numero 13 et 14 *)
+let ruleReturnBreak ast = 
+  let _ = pop (!e) in ast
+
+(* Numero 9 *)
+let ruleNoop ast =
+  let frame = pop (!e) in
+  match frame with
+    | InstrSeqFrame(i) -> i
+    | _ -> raise (Unknown_error_reference_interpretor "ruleNoop")
+
+(* Numero 3 *)
+let ruleAssignReturn ast =
+  match ast with
+  | ReturnNode (_,Some(expr)) -> 
+    let ve = value_of_expr expr (!g,!s) in
+    let frame = pop (!e) in
+    (match frame with 
+      | FuncCallReturnFrame(old_s,name) -> 
+        s := old_s;
+        let pa = find (!s) name in
+        pa := ve;
+        NoopNode
+      | _ -> raise (Unknown_error_reference_interpretor "ruleAssignReturn2"))
+  | _ -> raise (Unknown_error_reference_interpretor "ruleAssignReturn1")
+
+(* Numero 4 *)
+let ruleVoid ast =
+  let frame = pop (!e) in
+  match frame with 
+    | FuncCallVoidFrame(old_s) -> 
+      s := old_s;
+      NoopNode
+    | _ -> raise (Unknown_error_reference_interpretor "ruleVoid")
+
+let ruleFinalValue ast =
+  match ast with
+    | ReturnNode (_, None) -> None
+    | ReturnNode (_,Some(expr)) -> Some(value_of_expr expr (!g,!s))
+    | _ -> raise (Unknown_error_reference_interpretor "ruleFinalValue")
+
+let exec_step ast frame =
+  match (frame,ast) with 
+    | (_, InstrSeqNode(_,_)) -> ruleSeqInstr ast
+    | (_,BinaryNode (_,a,Assign,CallNode (_,namef,expr))) -> ruleCallFuncWithReturn a namef expr
+    | (_, BinaryNode (_,a,Assign,expr)) -> ruleAssignInstr a expr
+    | (_, CallNode (_,namef,expr)) -> ruleCallFuncVoid namef expr
+    | (_, IfthenelseInstrNode (_,cond,i1,i2)) -> ruleIfThenElseInstr cond i1 i2
+    | (_, WhileNode (pos,expr,i)) -> ruleWhile pos expr i
+    | (_, ReturnNode (_,Some (CallNode (_,_,_)))) -> raise (Unknown_error_reference_interpretor "notImplemented")
+    | (Some(InstrSeqFrame(_)), ReturnNode (_,_)) -> ruleReturnBreak ast
+    | (Some(InstrSeqFrame(_)), NoopNode) -> ruleNoop ast
+    | (Some(FuncCallReturnFrame(_,_)), ReturnNode (_,_)) -> ruleAssignReturn ast
+    | (Some(FuncCallVoidFrame(_)), ReturnNode (_,_)) -> ruleVoid ast
+    | (None,NoopNode) -> ReturnNode(Lexing.dummy_pos,None)
+    | (Some(FuncCallVoidFrame(_)), NoopNode) -> ReturnNode(Lexing.dummy_pos,None)
+    | (_,_) -> raise (Unknown_error_reference_interpretor_ (frame,ast))
+    
+let rec exec_prg ast =
+  if (is_empty (!e)) then
+    (match ast with
+      | ReturnNode (_,_) -> ruleFinalValue ast
+      | _ -> exec_prg (exec_step ast None))
+  else exec_prg (exec_step ast (Some(Stack.top (!e))))
+    
+let run_prg ast env_type start = 
+  (* Assignment of the type environment to the global variable envType *)
+  envType := env_type;
+  g := Hashtbl.create 100;
+  s := Hashtbl.create 100;
+  e := Stack.create (); 
+  (* Filling of the hash table with all the variables and functions *)
+  let rec aux tree =
+    match tree with
+        | ProgramNode (p1,p2) -> aux p1; aux p2
+        | FunctionNode (_,_,n,p,b) -> add (!g) n (ref (FuncVal(p,b)))
+        | GlobalVarDeclaNode(_,_,n,vnode) -> add (!g) n (ref (ruleValue vnode))
+        | _ -> ()
+    in aux ast;
+    (* Launch of the execution with the start function *)
+    match start with
+      | Some(_,name,expr) -> 
+        let f = !(Hashtbl.find !g name) in
+        let ve = value_of_expr expr (!g,!s) in
+        (match f with 
+          | FuncVal(param,BodyNode(_,decla,instr)) -> 
+            s := create_state param decla ve;
+            exec_prg instr
+          | _ -> raise (Unknown_error_reference_interpretor "run_prg2"))
+      | _ -> raise (Unknown_error_reference_interpretor "run_prg1")
---
+++ src/ExpressionInterpretor.ml
@@ -0,0 +1,224 @@
+open Ast
+
+type valueType =
+  | IntegerVal of int
+  | BooleanVal of bool
+  | StringVal of string
+  | CharVal of string
+  | ListVal of valueType list
+  | TupleVal of valueType list
+  | FuncVal of ast * ast
+  | ErrorVal
+
+exception Unknown_error_reference_interpretor of string
+exception Run_time_error of string
+
+(* string_of_val: valueType option -> string
+Function converting a value into a string
+Parameters:
+  - v: the value to convert
+Return: string representing the value
+*)
+let string_of_val v =
+  match v with
+    | None -> "unit"
+    | Some(vs) ->
+      let rec aux v =
+        match v with
+        | IntegerVal(i) -> string_of_int i 
+        | BooleanVal(b) -> string_of_bool b
+        | StringVal(s) -> "\"" ^ s ^ "\""
+        | CharVal(c) -> "\'" ^ c ^ "\'"
+        | ListVal([]) -> "[]"
+        | ListVal(l) -> "[" ^ (List.fold_left (fun s e ->  s ^ ", " ^ (aux e)) (aux (List.nth l 0)) (List.tl l)) ^ "]"
+        | TupleVal(t) -> "(" ^ (List.fold_left (fun s e ->  s ^ ", " ^ (aux e)) (aux (List.nth t 0)) (List.tl t)) ^ ")"
+        | _ -> "ERROR"
+      in aux vs 
+
+(* value_of_expr: (string, valueType ref) Hashtbl.t -> valueType
+Function calculating the value of an expression
+Parameters:
+  - expr: abstract syntax tree representing the expression
+  - state: hash table representing the state of all the variables  
+Return: a string corresponding to the expression
+*)
+let rec value_of_expr expr state =
+  match expr with
+    | BinaryNode (_,left,op,right) -> ruleBinary op left right state
+    | UnaryNode (_,op,expr) -> ruleUnary op expr state
+    | IfthenelseExprNode (_,cond,e1,e2) -> ruleIfThenElseExpr cond e1 e2 state
+    | ExprNode (_,e) -> value_of_expr e state
+    | ExprsNode (e,None) -> value_of_expr e state
+    | ExprsNode (e1,Some(e2)) -> ruleTupleExpr (ExprsNode (e1,Some(e2))) state
+    | ValueNode (v) -> ruleValue v
+    | AssignNode (_,a) -> ruleIdentifier a state
+    | _ -> raise (Unknown_error_reference_interpretor "value_of_expr")
+
+and
+
+
+(* ruleUnary: unary -> ast -> (string, valueType ref) Hashtbl.t -> valueType
+Function calculating the value of an unary operation
+Parameters:
+  - op: unary operator
+  - expr: abstract syntax tree representing the expression
+  - state: hash table representing the state of all the variables  
+Return: the result corresponding to the unary operation
+*)
+ruleUnary op expr state = 
+  let v = value_of_expr expr state in
+  (match v with
+    | IntegerVal(i) -> 
+      (match op with
+        | NegateInt -> IntegerVal(- i)
+        | Odd -> if ((i mod 2) == 1) then BooleanVal(true) else BooleanVal(false)
+        | Even -> if ((i mod 2) == 0) then BooleanVal(true) else BooleanVal(false)
+        | _ -> raise (Unknown_error_reference_interpretor "ruleUnary1")
+      ) 
+    | BooleanVal(b) ->
+      (match op with
+        | NegateBool -> BooleanVal((not b))
+        | _ -> raise (Unknown_error_reference_interpretor "ruleUnary2")
+      )
+    | ListVal([]) -> 
+      raise (Run_time_error "You cannot apply an operator to an empty list") 
+    | ListVal(t::q) -> 
+      (match op with
+        | Head -> t
+        | Tail -> ListVal(q)
+        | _ -> raise (Unknown_error_reference_interpretor "ruleUnary3")
+      )
+    | TupleVal(l) -> 
+      (match op with
+        | Fst -> List.nth l 0
+        | Snd -> List.nth l 1 
+        | _ -> raise (Unknown_error_reference_interpretor "ruleUnary4")
+      )
+    | _ -> raise (Unknown_error_reference_interpretor "ruleUnary")
+  )
+
+and
+
+
+(* ruleBinary: binary -> ast -> ast -> (string, valueType ref) Hashtbl.t -> valueType
+Function calculating the value of an binary operation
+Parameters:
+  - op: unary operator
+  - lexpr: abstract syntax tree representing the left expression
+  _ rexpr: abstract syntax tree representing the right expression
+  - state: hash table representing the state of all the variables  
+Return: the result corresponding to the binary operation
+*)
+ruleBinary op lexpr rexpr state = 
+  let vl = value_of_expr lexpr state in
+  let vr = value_of_expr rexpr state in  
+  (match (vl,vr) with
+    | (IntegerVal(il),IntegerVal(ir)) -> 
+      (match op with
+        | Equal -> BooleanVal(il == ir)
+        | Different -> BooleanVal(il != ir)
+        | Lesser -> BooleanVal(il < ir)
+        | Greater -> BooleanVal(il > ir)
+        | Add -> IntegerVal(il + ir)
+        | Substract -> IntegerVal(il - ir)
+        | Multiply -> IntegerVal(il * ir)
+        | Divide -> IntegerVal(il / ir)
+        | _ -> raise (Unknown_error_reference_interpretor "ruleBinary1")
+      )
+    | (BooleanVal(bl),BooleanVal(br)) ->
+      (match op with
+        | Equal -> BooleanVal(bl == br)
+        | Different -> BooleanVal(bl != br)
+        | Or -> BooleanVal(bl || br)
+        | And -> BooleanVal(bl && br)
+        | _ -> raise (Unknown_error_reference_interpretor "ruleBinary2")
+      ) 
+    | ((StringVal(sl),StringVal(sr)) | (CharVal(sl),CharVal(sr)))-> 
+      (match op with
+        | Equal -> BooleanVal(String.equal sl sr)
+        | Different -> BooleanVal(not (String.equal sl sr))
+        | _ -> raise (Unknown_error_reference_interpretor "ruleBinary3")
+      )
+    | (IntegerVal(i),TupleVal(t)) -> List.nth t i 
+    | _ -> raise (Unknown_error_reference_interpretor "ruleBinary")
+  )
+
+and 
+
+
+(* ruleIfThenElseExpr: ast -> ast -> ast -> (string, valueType ref) Hashtbl.t -> valueType
+Function calculating the value of an binary operation
+Parameters:
+  - cond_expr: abstract syntax tree representing the expression for the condition
+  - then_expr: abstract syntax tree representing the expression in the then branch
+  - else_expr: abstract syntax tree representing the expression in the else branch
+  - state: hash table representing the state of all the variables  
+Return: the result corresponding to the conditional expression
+*)
+ruleIfThenElseExpr cond_expr then_expr else_expr state =
+  let vcond = value_of_expr cond_expr state in
+  match vcond with
+    | BooleanVal(true) -> value_of_expr then_expr state
+    | BooleanVal(false) -> value_of_expr else_expr state
+    | _ -> raise (Unknown_error_reference_interpretor "ruleIfThenElseExpr")
+    
+and
+
+
+(* ruleTupleExpr: ast -> (string, valueType ref) Hashtbl.t -> valueType
+Function calculating the value of an expression tuple
+Parameters:
+  - exprs: abstract syntax tree representing the tuple of expressions
+  - state: hash table representing the state of all the variables  
+Return: the result corresponding to the expression tuple
+*)
+ruleTupleExpr exprs state =
+  TupleVal (
+  let rec aux exs l =
+    match exs with
+      | ExprsNode (e,None) -> List.rev ((value_of_expr e state)::l)
+      | ExprsNode (e1,Some(e2)) -> aux e2 ((value_of_expr e1 state)::l)
+      | _ -> raise (Unknown_error_reference_interpretor "ruleTupleExpr")
+  in aux exprs [])
+
+and
+
+
+(* ruleValue: ast -> (string, valueType ref) Hashtbl.t -> valueType
+Function converting a value node into a valueType
+Parameters:
+  - v: the abstract syntax tree representing the value
+  - state: hash table representing the state of all the variables  
+Return: the result corresponding to the value node
+*)
+ruleValue v =
+  let rec aux vs l =
+    (match vs with
+      | ValueSeqNode (val1,None) -> List.rev ((ruleValue val1)::l)
+      | ValueSeqNode (val1,Some(val2)) -> aux val2 ((ruleValue val1)::l)
+      | _ -> raise (Unknown_error_reference_interpretor "ruleValue2")) in
+  match v with
+    | ValueNode (IntegerNode (_,i)) -> IntegerVal(i)
+    | ValueNode (CharNode (_,c)) -> CharVal(c)
+    | ValueNode (StringNode (_,s)) -> StringVal(s)
+    | ValueNode (TrueNode(_)) -> BooleanVal(true)
+    | ValueNode (FalseNode(_)) -> BooleanVal(false)
+    | ValueNode (EmptyList) -> ListVal([])
+    | ValueNode (ValueSeqNode (v1,v2)) -> ListVal (aux (ValueSeqNode(v1,v2)) [])
+    | ValueSeqNode (_,_) -> TupleVal (aux v [])
+    | _ -> raise (Unknown_error_reference_interpretor "ruleValue")
+and
+
+
+(* ruleIdentifier: ast -> (string, valueType ref) Hashtbl.t -> valueType
+Function finding the value associated to an identifier
+Parameters:
+  - a: the abstract syntax tree representing the assignable
+  - state: hash table representing the state of all the variables  
+Return: the result corresponding to the assignable
+*)
+ruleIdentifier a (global,local) = 
+  let n = Hashtbl.find_opt local a in
+  match n with
+    | None -> !(Hashtbl.find global a)
+    | Some(name) -> !name
